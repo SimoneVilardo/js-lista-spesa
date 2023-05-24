821d86165d3239f6f91df2d398c50422cc76cda0
SimoneVilardo <simovila95@gmail.com>
2023-05-24 14:32:11 +0200

    Aggiunti commenti

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// ARRAY SPESA
 elementi_spesa = [
     'Sedano',
     'Carote',
@@ -8,13 +9,15 @@ elementi_spesa = [
     'Provola',
     'Pasta Mista'
 ]
-
+// RECUPERO CON getElementbyId l'id dato all'ul dell'html
 const lista_spesa = document.getElementById('lista-spesa');
 
+// creo una variabile con indice 0
 let elementi = 0;
 
 while(elementi < elementi_spesa.length) {
     
+    // creo le li attraverso una variabile e le mostro a schermo con innerHTML
     let contenuto = `<li>${elementi_spesa[elementi]}</li>`
     lista_spesa.innerHTML += contenuto
 
